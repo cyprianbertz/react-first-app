3e8f09ce51266e9430946b633793e61cf926d26d
Patrycja Kur <patrycja.kur@outlook.com>
2019-07-13 18:36:54 +0200

    Updates

---
+++ precache-manifest.66fe1083a7c99bc940749e0c97701969.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfdedb856edd8a7b323cf942a67de2c",
+    "url": "/react-first-app/index.html"
+  },
+  {
+    "revision": "b0b82678f02e494b6c08",
+    "url": "/react-first-app/static/css/main.f8cdb93c.chunk.css"
+  },
+  {
+    "revision": "c5661d07f8f741381f9b",
+    "url": "/react-first-app/static/js/2.38d33b13.chunk.js"
+  },
+  {
+    "revision": "b0b82678f02e494b6c08",
+    "url": "/react-first-app/static/js/main.331fc121.chunk.js"
+  },
+  {
+    "revision": "561ffc8d17bb1db5b2f7",
+    "url": "/react-first-app/static/js/runtime~main.0b008e58.js"
+  }
+]);
